6d0277b1a724d5fd0c88d4396d9a42abdcdd4594
Terormumuli <terahmumuli@gmail.com>
2022-08-23 22:05:22 +0300

    Self Count Sentence

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+      self.end_with? "!"
   end
 
   def count_sentences
+      split_sent_array = self.split(/[.!?]/)
 
+      split_sent_array.delete_if {|elem| elem == ""}
+
+      no = split_sent_array.count
   end
 end
